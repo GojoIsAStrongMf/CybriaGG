870c76854c5b4f12c95a9d1eba35e31280ec8bfd
Satoru Gojo <gojothestrongone@gmail.com>
2024-11-07 23:52:07 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
 const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-const app = express();
+if (cluster.isMaster) {
+  console.log(`Slave ${process.pid} is running`);
 
-const nggUrl = 'https://mathsspot.com';
-
-const proxy = createProxyMiddleware({
-  target: nggUrl,
-  changeOrigin: true,
-  secure: true,
-  logLevel: 'debug',
-  router: function(req) {
-    if (req.headers.host === 'mathsspot.com') {
-      req.headers['X-Forwarded-For'] = ''; 
-      req.headers['X-Real-IP'] = '';
-      req.headers['Via'] = '';
-    }
-    return nggUrl;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
   }
-});
 
-app.use('/', proxy);
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Slave ${worker.process.pid} died`);
+    cluster.fork();
+  });
+} else {
+  const app = express();
 
-const port = process.env.PORT || 443;
-app.listen(port, () => {
-  console.log(`CybriaGG is running on port ${port}`);
-});
+  const nggurl = 'https://educationbluesky.com';
+
+  const proxy = httpProxy.createProxyServer({
+    target: nggurl,
+    changeOrigin: true,
+  });
+
+  app.use('/', (req, res) => {
+    console.log(`Incoming Requests: ${req.url}`);
+    proxy.web(req, res);
+  });
+
+  app.use((req, res, next) => {
+    res.setHeader('Content-Security-Policy', 'default-src "self"; style-src "self"');
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader('X-XSS-Protection', '1; mode=block');
+    res.setHeader('Content-Type', 'text/html');
+
+    next();
+  });
+
+  const server = http.createServer(app);
+
+  const PORT = 8080;
+  server.listen(PORT, () => {
+    console.log(`CybriaGG Slave ${process.pid} has been successfully run! On Port ${PORT}`);
+  });
+
+  process.on('unhandledRejection', (reason, promise) => {
+    console.error(`Slave ${process.pid} detected unhandled rejections:`, promise, 'reason:', reason);
+  });
+
+  process.on('uncaughtException', (err) => {
+    console.error(`Slave ${process.pid} uncaught exception:`, err);
+  });
+}
